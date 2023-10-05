8744a9d18cabfd99f02a81514e2ddf8dbe12e3ef
ahmed-my <yahaya.mymohammed@gmail.com>
2023-10-05 21:45:40 +0000

    import and modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,9 +8,13 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     operator = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(operator.keys()):
+    operator_symbol = sys.argv[2]
+
+    if operator_symbol not in operator:
         print("Unknown operator. Available operations: +, -, * and /")
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, operator[sys.argv[2]](a, b)))
+
+    result = operator[operator_symbol](a, b)
+    print("{} {} {} = {}".format(a, sys.argv[2], b, result))
