6e9657b949daa8c83b4ccb3ab3a33de273fbfbed
ahmed-my <yahaya.mymohammed@gmail.com>
2023-10-05 12:17:03 +0000

    import and module

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add(a, b):
+    """ function to add two numbers
+    Args:
+        a: first number
+	b: second number
+    Returns: the value of a + b
+    """"
+    return (a + b)
