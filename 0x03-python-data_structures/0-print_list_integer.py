116429540176f3850814c5f34abae571b71b02f2
ahmed-my <yahaya.mymohammed@gmail.com>
2023-10-07 09:26:34 +0000

    Python data structures

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if __name__ == "__main__":
         length = len(my_list)
         for i in range(length):
-            print("{}".format(my_list[i]))
+            print("{:d}".format(my_list[i]))
