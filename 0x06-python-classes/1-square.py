19b688f7ba13f5c92be6b9a96ad566adc09f504f
ahmed-my <yahaya.mymohammed@gmail.com>
2023-10-24 18:05:10 +0000

    python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" A sqaure module """
+
+
+class Sqaure:
+    """ a Class Square that defines a square by size """
+
+    def __init__(self, size):
+        """ Constructor
+        args:
+            size: length of the square side
+       	"""
+        self.__size = size
+    
