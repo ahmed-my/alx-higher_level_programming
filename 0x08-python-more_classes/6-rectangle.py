fb628ca7774cb2b178319b1b997dd4db5e7f45f3
ahmed-my <yahaya.mymohammed@gmail.com>
2023-10-30 22:23:14 +0000

    more on python classes

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle:
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle."""
-        type(self).number_of_instance += 1
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
