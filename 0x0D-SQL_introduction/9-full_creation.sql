d2f062f85efae5439e3d45a5c622fa40824fb84e
ahmed-my <yahaya.mymohammed@gmail.com>
2023-12-12 15:53:28 +0000

    SQL introduction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,7 +6,7 @@
 -- id = 2, name = "Alex", score = 3
 -- id = 3, name = "Bob", score = 14
 -- id = 4, name = "George", score = 8
-CREATE IF NOT EXISTS TABLE second_table (
+CREATE TABLE IF NOT EXISTS second_table (
     id INT AUTO_INCREMENT,
     name VARCHAR(256),
     score INT
