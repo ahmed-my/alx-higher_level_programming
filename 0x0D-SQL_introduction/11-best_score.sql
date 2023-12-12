4b8cd5709d25d2a23e08e79cac559042d37a4c74
ahmed-my <yahaya.mymohammed@gmail.com>
2023-12-12 16:31:40 +0000

    SQL introduction

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,8 @@
+-- Script that lists all records with a score >= 10 in the 
+-- second_table of the database hbtn_0c_0
+-- Results should display both the score and name (in this order)
+-- Records should be ordered by score (top first)
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score;
