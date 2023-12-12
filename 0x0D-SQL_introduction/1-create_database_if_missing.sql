213486c84c5f0954e2ceba6993de8ef020186fff
ahmed-my <yahaya.mymohammed@gmail.com>
2023-12-12 12:58:07 +0000

    SQL introduction

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
 -- Script that creates the database hbtn_0c_0
-CREATE DATABASE hbtn_0c_0;
+-- if the database hbtn_0c_0 already exists, script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
