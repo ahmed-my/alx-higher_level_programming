5e10db269a4cc208c62ef7bac63523ee26195913
ahmed-my <yahaya.mymohammed@gmail.com>
2023-12-12 18:19:00 +0000

    SQL introduction

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,8 @@
+-- Script that lists all records of the table second_table
+-- of the database
+-- Don't list rows without a name value
+-- Result should display the score and the name (in this order)
+-- Records should be listed by descending score
+SELECT score, name
+FROM second_table
+ORDER BY score DESC
