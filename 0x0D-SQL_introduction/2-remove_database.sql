c3738c5d604c61f99683663a3ba963ef7dedab24
ahmed-my <yahaya.mymohammed@gmail.com>
2023-12-12 13:26:40 +0000

    SQL introduction

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 --script that deletes the database hbtn_0c_0 in my MySQL server
 -- if the database hbtn_0c_0 doesn't exist, script should not fail
-DROP DATABASE IF EXIST hbtn_0c_0;
+DROP IF EXISTS hbtn_0c_0;
