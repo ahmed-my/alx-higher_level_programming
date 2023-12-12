f77f1c94bb3257df56a472caee12acb45dca711c
ahmed-my <yahaya.mymohammed@gmail.com>
2023-12-12 13:47:14 +0000

    SQL introduction

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
 -- Script that creates a table called first_table
-CREATE TABLE IF EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);
+CREATE TABLE IF EXISTS first_table ( id INT, name VARCHAR(256));
