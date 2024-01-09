043f3848a99596d075ed391ebec43a4be7ecf2f7
ahmed-my <yahaya.mymohammed@gmail.com>
2024-01-09 14:43:47 +0000

    Objects, Scopes and Closures

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (l, b) {
+    this.length = l;
+    this.breadth = b;
+  }
+}
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,2 @@
+# 0X13. Objects, Scopes and Closures
+Javascript on Objects, Scopes and Closures
