61a630e32486d10c588ac50b00516ddd5facd4e6
ahmed-my <yahaya.mymohammed@gmail.com>
2024-01-09 00:37:07 +0000

    javascript

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,4 +1,16 @@
 #!/usr/bin/node
+function factorial (n) {
+  if (n < 0) {
+    return (-1);
+  }
+  if (n === 0 || isNaN(n)) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
+}
+
+console.log(factorial(Number(process.argv[2])));
+
 /*
 const factorial = (n) => {
   if (isNaN(n)) {
@@ -8,7 +20,7 @@ const factorial = (n) => {
 };
 const arg = parseInt(process.argv[2], 10);
 console.log(factorial(arg));
-*/
+
 const factorial = function fac(n) {
   return n < 2 ? 1 : n * fac(n - 1);
 };
@@ -20,3 +32,4 @@ if (!isNaN(arg)) {
 } else {
   console.log(1);
 }
+*/
