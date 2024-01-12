94540a25f644ee48b4ca65f89349c681548a744f
ahmed-my <yahaya.mymohammed@gmail.com>
2024-01-12 15:23:13 +0000

    Objects Relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+""" list to display all states from the database """
 import MySQLdb
 import sys
 
+from sqlalchemy import create_engine, Column, Integer, String, Sequence
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+user = sys.argv[1]
+passwd = sys.argv[2]
+host = "localhost"
+db = sys.argv[3]
+
+engine = create_engine(f"mysql+mysqldb://{user}:{passwd}@{host}/{db}",
+                       pool_pre_ping=True)
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, Sequence('state_id_seq'), primary_key=True)
+    name = Column(String(256))
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+states = session.query(State).all()
+for state in states:
+    print((state.id, state.name))
+
+session.close()
