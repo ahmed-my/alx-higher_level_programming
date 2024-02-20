f182217f2a250bdc88aadf6023c25048a356787a
ahmed-my <yahaya.mymohammed@gmail.com>
2024-02-20 12:45:58 +0000

    javascript web scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
 if (!filePath || !contentToWrite) {
-  console.error('provide first and second argument from command line');
+  process.exit(1);
 }
 fs.writeFile(filePath, contentToWrite, 'utf8', (err) => {
   if (err) {
