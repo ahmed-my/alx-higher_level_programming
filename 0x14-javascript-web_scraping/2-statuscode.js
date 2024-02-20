90ced2a39a87b4c71d7541375e54429941cb75bc
ahmed-my <yahaya.mymohammed@gmail.com>
2024-02-20 13:51:26 +0000

    javascript web scraping

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const request = require('request');
+const url = process.argv[2];
+if (!url) {
+  process.exit(1);
+}
+request.get(url, (err, response) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
