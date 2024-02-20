467befed94887ec14b3fad2f1d0d89623767b348
ahmed-my <yahaya.mymohammed@gmail.com>
2024-02-20 23:47:33 +0000

    javascript web scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-// web scrapper
-
 const request = require('request');
-
-request.get(process.argv[2], { json: true }, (error, response, body) => {
+const apiUrl = process.argv[2];
+request.get(apiUrl, { json: true }, (error, response, body) => {
   if (error) {
     console.log(error);
     return;
