6b2f5203680f649c74ed9a7194822abc141aff37
ahmed-my <yahaya.mymohammed@gmail.com>
2024-02-20 12:43:07 +0000

    javascript web scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,6 +3,9 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 const contentToWrite = process.argv[3];
+if (!filePath || !contenToWrite) {
+  console.error('provide first and second argument from command line');
+}
 fs.writeFile(filePath, contentToWrite, 'utf8', (err) => {
   if (err) {
     console.error(err);
