fcfca7769aecda04afc3bf77517e5abff4940e94
ahmed-my <yahaya.mymohammed@gmail.com>
2024-02-20 14:33:36 +0000

    javascript web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-const movie_id = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/' + movie_id;
+const movieId = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 request(url, (error, response, body) => {
   console.log(error || JSON.parse(body).title);
 });
