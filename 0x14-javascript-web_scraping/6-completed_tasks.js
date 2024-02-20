811b24240f76aec0bfd04d8b6abaeae83e80569f
ahmed-my <yahaya.mymohammed@gmail.com>
2024-02-20 19:53:47 +0000

    javascript web scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,33 +1,38 @@
 #!/usr/bin/node
 
-const request = require('request');
+const request = require('request-promise');
+
+async function fetchCompletedTasks(apiUrl) {
+  try {
+    const body = await request(apiUrl);
+    const todos = JSON.parse(body);
+
+    const completed = {};
+
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        completed[todo.userId] = (completed[todo.userId] || 0) + 1;
+      }
+    });
+
+    const output = `{${Object.entries(completed).map(([key, value]) => ` '${key}': ${value}`).join(',\n ')} }`;
+
+    console.log(Object.keys(completed).length > 2 ? output : completed);
+  } catch (error) {
+    if (error.statusCode) {
+      console.error(`Error: API request failed with status code ${error.statusCode}`);
+    } else {
+      console.error('Error:', error.message);
+    }
+  }
+}
 
 const apiUrl = process.argv[2];
 
-request(apiUrl, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    try {
-      const todos = JSON.parse(body);
+if (!apiUrl) {
+  console.error('Please provide the API URL as the first argument.');
+  process.exit(1);
+}
 
-      const completed = {};
+fetchCompletedTasks(apiUrl);
 
-      todos.forEach((todo) => {
-        if (todo.completed) {
-          if (completed[todo.userId] === undefined) {
-            completed[todo.userId] = 1;
-          } else {
-            completed[todo.userId]++;
-          }
-        }
-      });
-
-      const output = `{${Object.entries(completed).map(([key, value]) => ` '${key}': ${value}`).join(',\n ')} }`;
-
-      console.log(Object.keys(completed).length > 2 ? output : completed);
-    } catch (parseError) {
-      console.error('Error parsing JSON:', parseError);
-    }
-  } else {
-    console.error('Error:', error);
-  }
-});
