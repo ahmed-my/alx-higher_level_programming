413e474854a8db7b0367ab421a013c62001a3193
ahmed-my <yahaya.mymohammed@gmail.com>
2024-02-20 11:57:18 +0000

    javascript web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,6 @@
 
 const fs = require('fs');
 const filepath = process.argv[2];
-if (!filepath) {
-	process.exit(1);
-}
 fs.readFile(filepath, 'utf8', (err, content) => {
   if (err) {
     console.error(err);
