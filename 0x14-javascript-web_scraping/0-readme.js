4175ef035f84cd4a7fd9d200b4c5e91e0662fbec
ahmed-my <yahaya.mymohammed@gmail.com>
2024-02-20 11:53:56 +0000

    javascript web scraping

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filepath = process.argv[2];
+if (!filepath) {
+	process.exit(1);
+}
+fs.readFile(filepath, 'utf8', (err, content) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(content);
+  }
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,2 @@
+# 0x14 JavaScript Web Scraping
+Web Scraping in JavaScript using the request module
