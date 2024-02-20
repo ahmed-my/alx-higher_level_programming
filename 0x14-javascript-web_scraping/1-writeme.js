72e58c380b509f599e0eb88cd0aad83e685fcf1f
ahmed-my <yahaya.mymohammed@gmail.com>
2024-02-20 12:39:02 +0000

    javascript web scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 const content_to_write = process.argv[3];
-fs.writeFile(filePath, content_to_write, 'utf-8', (err) => {
+fs.writeFile(filePath, content_to_write, 'utf8', (err) => {
   if (err) {
     console.error(err);
   }
